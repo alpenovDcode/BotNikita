40a36928bf261745ff63f7886f6e1af58ed21284
nikitalook1 <nikitabaramidze.2003617@gmail.com>
2024-05-30 18:56:13 +0300

    Remove OpenAI API Key

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
+import openai
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 API_TOKEN = "6786447329:AAGBjuy7GKD1TK-mNYqkO8h2EgH3FNDdtPI"
-ADMINS = [6139227440]
+ADMINS = [6139227440, 6977471553]
+openai.api_key = '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'
+
+model_name = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+def generate_response(prompt):
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(inputs.input_ids, max_length=200)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response

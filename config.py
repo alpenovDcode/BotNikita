9bffc6577b20a30ae275092072a971461466079d
nikitalook1 <nikitabaramidze.2003617@gmail.com>
2024-05-30 18:57:47 +0300

    new

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 API_TOKEN = "6786447329:AAGBjuy7GKD1TK-mNYqkO8h2EgH3FNDdtPI"
 ADMINS = [6139227440, 6977471553]
-openai.api_key = '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'
+
 
 model_name = "gpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
